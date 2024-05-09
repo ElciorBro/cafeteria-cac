5e02bc9b1ad7ce171a51f759e4920e461833a159
ElciorBro <elias.barboza.eze@gmail.com>
2024-05-09 16:31:23 -0300

    css changes 05 working on animation

---
+++ proyecto-cafeteria/js/home.js
@@ -0,0 +1,31 @@
+window.addEventListener('scroll', function() {
+    var divPrincipalContent = document.getElementById('principal-content');
+    var divPos = divPrincipalContent.getBoundingClientRect().top;
+    var screenHeight = window.innerHeight;
+  
+    // Agregar la clase de animación cuando el div esté visible en pantalla
+    if (divPos < screenHeight * 0.75) { // Cambia 0.75 por el porcentaje que desees
+      divPrincipalContent.classList.add('animacion-in');
+      divPrincipalContent.classList.remove('animacion-out')
+    } else {
+      divPrincipalContent.classList.remove('animacion-in');
+      divPrincipalContent.classList.add('animacion-out')
+    }
+});
+
+// funcion para frase
+
+// window.addEventListener('scroll', function() {
+//     var divFrase = document.getElementById('frase');
+//     var divFrasePos = divFrase.getBoundingClientRect().top;
+//     var screenHeight = window.innerHeight;
+
+//     if (divFrasePos < screenHeight *0.75) {
+//         divFrase.classList.add('aparecer');
+//         divFrase.classList.remove('desaparecer');
+//     } else {
+//         divFrase.classList.remove('aparecer');
+//         divFrase.classList.add('desaparecer');
+//     }
+// })
+
