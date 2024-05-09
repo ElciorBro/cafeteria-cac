fdc5bb6557b5b8307b8e0d60fd43cc811b08f3d8
ElciorBro <elias.barboza.eze@gmail.com>
2024-05-09 19:24:01 -0300

    js, css changes 07 animation and js changes

--- proyecto-cafeteria/js/home.js
+++ proyecto-cafeteria/js/home.js
@@ -1,31 +1,47 @@
-window.addEventListener('scroll', function() {
-    var divPrincipalContent = document.getElementById('principal-content');
-    var divPos = divPrincipalContent.getBoundingClientRect().top;
-    var screenHeight = window.innerHeight;
-  
-    // Agregar la clase de animación cuando el div esté visible en pantalla
-    if (divPos < screenHeight * 0.75) { // Cambia 0.75 por el porcentaje que desees
-      divPrincipalContent.classList.add('animacion-in');
-      divPrincipalContent.classList.remove('animacion-out')
-    } else {
-      divPrincipalContent.classList.remove('animacion-in');
-      divPrincipalContent.classList.add('animacion-out')
-    }
-});
+const fadeAnimation = (elementId) => {
+  var element = document.getElementById(elementId);
+  var elementPos = element.getBoundingClientRect().top;
+  var screenHeight = window.innerHeight;
+
+  // Agregar la clase de animación cuando el div esté visible en pantalla
+  if (elementPos < screenHeight * 0.75) { // Cambia 0.75 por el porcentaje que desees
+    element.classList.add('animacion-in');
+    element.classList.remove('animacion-out')
+  } else {
+    element.classList.remove('animacion-in');
+    element.classList.add('animacion-out')
+  }
+}
+
+const moveRight = (elementId) => {
+  var element = document.getElementById(elementId);
+  var elementPos = element.getBoundingClientRect().top;
+  var screenHeight = window.innerHeight;
+
+  if (elementPos < screenHeight *0.75) {
+      element.classList.add('aparecer');
+      element.classList.remove('desaparecer');
+  } else {
+      element.classList.remove('aparecer');
+      element.classList.add('desaparecer');
+  }
+}
+
+const cargar = () => {
+  var element = document.getElementById('descripcion');
+  element.classList.add('animacion-init')
+}
+
+// Funcion para temaios
+
+window.addEventListener('scroll',() => {
+  fadeAnimation('principal-content');
+})
 
 // funcion para frase
 
-window.addEventListener('scroll', function() {
-    var divFrase = document.getElementById('frase');
-    var divFrasePos = divFrase.getBoundingClientRect().top;
-    var screenHeight = window.innerHeight;
-
-    if (divFrasePos < screenHeight *0.75) {
-        divFrase.classList.add('aparecer');
-        divFrase.classList.remove('desaparecer');
-    } else {
-        divFrase.classList.remove('aparecer');
-        divFrase.classList.add('desaparecer');
-    }
+window.addEventListener('scroll', () => {
+  moveRight('frase')
 })
 
+window.onload = cargar()
